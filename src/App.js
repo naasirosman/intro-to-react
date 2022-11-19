bbcfb7e71497aa88a6bc9effb2e83e1d153b8cce
naasirosmanzoro <naasir.osman@zoro.co.uk>
2022-11-19 14:06:54 +0000

    working without JSX

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", {}, [ // creates div
-    React.createElement("h1", { id: "title"}, "Adopt Me!"), // inside of it h1 of id "title"
-    React.createElement(Pet, {  // and pet via Pet.js fn
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
+    return React.createElement('div', {}, [
+        // creates div
+        React.createElement('h1', { id: 'title' }, 'Adopt Me!'), // inside of it h1 of id "title"
+        React.createElement(Pet, {
+            // and pet via Pet.js fn
+            name: 'Luna',
+            animal: 'Dog',
+            breed: 'Havanese',
+        }),
+        React.createElement(Pet, {
+            name: 'Pepper',
+            animal: 'Bird',
+            breed: 'Cockatiel',
+        }),
+        React.createElement(Pet, {
+            name: 'Doink',
+            animal: 'Cat',
+            breed: 'Mix',
+        }),
+    ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
